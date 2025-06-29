d6775639ee2624d8d7541a2bca6003eedcddc99d
admain <admain@gmail.com>
2025-06-29 22:24:35 +0530

    categoriser.py

---
+++ app/categoriser.py
@@ -0,0 +1,38 @@
+import openai
+import logging
+from app.config_loader import load_config
+
+config = load_config()
+openai.api_key = config["OPENAI_API_KEY"]
+model_name  = config["MODEL_NAME"]
+
+logging.basicConfig(level= logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def categorize_email(text):
+    try: 
+        logging.info("Categorizing Email...")
+        
+        promt = (
+            "Categorize the email as one of the following: "
+            "[Urgent, Normal, Spam]. Reply with the only category name."
+        )
+        response = openai.CahtCompletion.create(
+            model = model_name,
+            messages = [
+                {"role": "system", "content": promt}
+                {"role": "user", "content": text}
+            ],
+            temperature = 0.2
+            max_token = 10
+        )
+        category = response['choice'][0]['message']['content'].strip()
+        logging.info(f"Email categorized as {category}")
+        return category
+    
+    except openai.error.OpenAIError as e:
+        logging.error(f"OpenAI API error during categorization: {e}")
+        return "Unknown"
+    
+    except Exception as e:
+        logging.error(f"Unexpected error during categorization: {e}")
+        return "Unknown"
