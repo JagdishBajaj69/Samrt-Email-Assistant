dae1fd72c8e97de0b568c577ddcf819bda67df84
admain <admain@gmail.com>
2025-07-13 00:10:20 +0530

    added storage.py function to handle file storage

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from app.email_client import EmailClient
 from app.summarize import summarize_text
 from app.categoriser import categorize_email
 from app.auto_reply import generate_reply
+from app.storage import load_processed_emails, save_processed_email
 import logging
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+processed_emails = load_processed_emails()
 
 def main():
-    logging.info(f"Smart Email Assistant started.")
+    logging.info("Smart Email Assistant started.")
 
     client = EmailClient()
-    try :
+    try:
         client.login()
         logging.info("Logged in successfully!")
     except Exception as e:
@@ -21,25 +23,37 @@ def main():
     if not emails:
         logging.info("No unread emails found.")
         return
-    
-    for email in emails :
+
+    for email in emails:
+        email_id = email.get("message_id") or f"{email['subject']}_{email['from']}"
+        if email_id in processed_emails:
+            logging.info("Email already processed. Skipping.")
+            continue
+
         logging.info(f"Processing email from: {email['from']} — Subject: {email['subject']}")
-        
+
         summary = summarize_text(email["body"])
-        logging.info(f"Summary:{summary}")
-        
+        logging.info(f"Summary: {summary}")
+
         category = categorize_email(email["body"])
         logging.info(f"Category: {category}")
-        
-        reply = generate_reply(email["body"], category)   
-        
+
+        reply = generate_reply(email["body"], category)
+
+        if reply is None:
+            logging.info("Skipping spam or unknown category email.")
+            continue
+
         try:
             client.send_email(email["from"], f"Re: {email['subject']}", reply)
             logging.info("Reply sent successfully.")
         except Exception as e:
             logging.error(f"Failed to send reply: {e}")
-        
-    logging.info ("All Emails processed")
+            continue  # Don't mark it as processed if reply failed
 
-if __name__== "__main__":
+        save_processed_email(email_id)
+
+    logging.info("All Emails processed.")
+
+if __name__ == "__main__":
     main()
